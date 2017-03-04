0c74c0fdd6baf2061ac04413cb280ceab8ae3cc3
Shoaib Bhimani <shoaibbhimani1392@gmail.com>
2017-03-04 17:29:45 +0530

    React Setup

---
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -0,0 +1,53 @@
+import React, { PropTypes } from 'react';
+
+
+var Foo = function(props){
+  return(
+      <h1>{props.name}</h1>
+  )
+};
+
+export default class HelloWorld extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired, // this is passed from the Rails view
+  };
+
+  /**
+   * @param props - Comes from your rails view.
+   * @param _railsContext - Comes from React on Rails
+   */
+  constructor(props, _railsContext) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+    this.state = { name: 'Shaoib' }
+  }
+
+  updateName = (name) => {
+    this.setState({ name });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>
+          <Foo name={this.state.name}/>
+          Hello, {this.state.name}!
+        </h3>
+        <hr />
+        <form >
+          <label htmlFor="name">
+            Say hello to:
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={(e) => this.updateName(e.target.value)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
