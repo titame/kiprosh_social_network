e7707c963083c69eaead800f03d5e884bf596084
titame <tushar.titame@kiprosh.com>
2017-03-04 19:31:59 +0530

    versioned api structure

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,4 @@
+class EventsController < ActionController::Base
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: "json" }  do
+    namespace :v1 do
+      get "events", to: "events#index"
+    end
+  end
 end
